985adca71494caa26a06ae0cb21e976b606d7ebb
Donaji13 <toltekochoa12@gmail.com>
2014-11-10 13:23:08 -0800

    added i pad media querie

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,8 +1,8 @@
 <?php
-	$title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING); //inputing post to the user
+	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);//inputing post to the user
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING); // grabing from title
 
 
 
-	echo "<p>Title: $title</p>";
-	echo "<p>Post: $post</p>";
+	echo "<p>Title: $title</p>";//echoing out the title
+	echo "<p>Post: $post</p>";//echoing out the post
